304dd32e30ed8e1f04a7391ecfd972e42605718c
usernamenoahfoster <48572872+usernamenoahfoster@users.noreply.github.com>
2022-07-18 20:29:18 -0700

    addeed files for encoder and runner

---
+++ brainfuckify.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc, char** argv){
+    string text;
+    
+    switch (argc) {
+        case 1:
+            getline(cin, text);
+        default:
+
+    }
+}
+
